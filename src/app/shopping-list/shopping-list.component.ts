36c62f246b99ed2dc156cb00d56abf4ef039078d
linkavich14 <5630335+linkavich14@users.noreply.github.com>
2018-10-08 10:36:56 +1100

    Added form functionality, fixed bugs

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import { Recipe } from '../recipes/recipe.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {  
-  ingredients: Ingredient[]
+export class ShoppingListComponent implements OnInit, OnDestroy {  
+  ingredients: Ingredient[];
+  private subscription: Subscription
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe(
+    this.subscription = this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       });
+  }
+
+  onEditItem(index:number){
+    this.slService.startedEditing.next(index);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
